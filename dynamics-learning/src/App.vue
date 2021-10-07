7e81834a35d44baf7d508cbed62d1ec9242efed8
Alphailosia <steph13.desire@gamail.com>
2021-10-07 12:42:25 +0200

    ajout gitignore dans partie serveur + serveur fonctionnel

--- dynamics-learning/src/App.vue
+++ dynamics-learning/src/App.vue
@@ -36,21 +36,15 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
--- serveur/app.js
+++ serveur/app.js
@@ -13,6 +13,12 @@ const io = require('socket.io')(server, {
    }
 })
 
+io.on('connection', (socket) => {
+
+    console.log(`Connecté au client ${socket.id}`);
+
+});
+
 server.listen(3000, function () {  // mettre process.env.PORT à la place de 3000 pour le deploiement en ligne
     console.log('Votre app est disponible sur localhost:3000 !')
  })
