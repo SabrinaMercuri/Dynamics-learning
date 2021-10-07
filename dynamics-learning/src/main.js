eaa2a2809807bdcdabd0c65c2203e59f070d7424
Alphailosia <steph13.desire@gamail.com>
2021-10-07 12:25:08 +0200

    ajout structure du projet client/serveur + config

--- dynamics-learning/src/main.js
+++ dynamics-learning/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
+
+
+export const SocketInstance = socketio('http://localhost:3000');
+//export const SocketInstance = socketio('https://monopolymiage.herokuapp.com/'); pour la mise en ligne 
+
+Vue.use(new VueSocketIO({
+  connection: SocketInstance
+}))
 
 Vue.config.productionTip = false
 
