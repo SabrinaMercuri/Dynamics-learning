268cad3ff2b8180ea36dc28a33ea3ee9ee406356
Alphailosia <steph13.desire@gamail.com>
2021-10-08 12:00:33 +0200

    ajout du template des questions et des réponses

--- dynamics-learning/src/components/etudiant/reponseQcm.vue
+++ dynamics-learning/src/components/etudiant/reponseQcm.vue
@@ -1,61 +1,90 @@
 <template>
   <v-app>
-    <h1>{{ qcm.nom }}</h1>
-    <v-card v-for="(question,index) in qcm.questions" :key="index">
-        <h2>question.intitule</h2>
-
-    </v-card>
+    <form>
+      <h1>QCM - {{ qcm.nom }}</h1>
+      <v-card v-for="(question, index) in qcm.questions" :key="index">
+        <div v-if="question.trueRep.length == 1">
+          <h2>{{ index + 1 }} - {{ question.intitule }}</h2>
+          <p>1 réponse</p>
+          <v-radio-group>
+          <v-radio v-for="(reponse,index2) in getAnswer(question)" :key="index2"
+              :label="reponse"
+              :color="isTrueRep(question,reponse)"
+            ></v-radio>
+            </v-radio-group>
+        </div>
+        <div v-if="question.trueRep.length !== 1">
+          <h2>{{ index + 1 }} - {{ question.intitule }}</h2>
+          <p>plusieurs réponses</p>
+          <div v-for="(reponse,index3) in getAnswer(question)" :key="index3">
+              <v-checkbox
+              :label="reponse"
+              :color="isTrueRep(question,reponse)"
+            ></v-checkbox>
+          </div>
+        </div>
+      </v-card>
+    </form>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
-    qcm : {
-        nom:"sossur",
-        mdp:"",
-        questions:[
-            {
-                intitule:"sossur 1 ?",
-                falseRep:[
-                    "1",
-                    "2"
-                ],
-                trueRep:[
-                    "3",
-                    "4"
-                ]
-            },
-            {
-                intitule:"sossur 2 ?",
-                falseRep:[
-                    "1",
-                    "2"
-                ],
-                trueRep:[
-                    "3",
-                    "4"
-                ]
-            },
-            {
-                intitule:"sossur 3 ?",
-                falseRep:[
-                    "1",
-                    "2"
-                ],
-                trueRep:[
-                    "3",
-                    "4"
-                ]
-            },
-        ]
-    }
+    qcm: {
+      nom: "Pokemon",
+      mdp: "",
+      questions: [
+        {
+          intitule: "Combien d'évolution à évoli ?",
+          falseRep: ["1", "5", "7"],
+          trueRep: ["8"],
+        },
+        {
+          intitule: "Quel type est éfficace contre le type dragon ?",
+          falseRep: ["acier", "feu"],
+          trueRep: ["dragon", "glace", "sol"],
+        },
+        {
+          intitule:
+            "Comment s'appel les protagonise de la team rocket les plus connus ?",
+          falseRep: ["Boule et Bill", "Azur et Asmar"],
+          trueRep: ["Jessie et James"],
+        },
+      ],
+    },
   }),
+  methods: {
+    getAnswer: function (question) {
+      let tabRep = question.falseRep;
+      for (let q of question.trueRep) {
+        tabRep[tabRep.length] = q;
+      }
+      return this.shuffleArray(tabRep);
+    },
+    shuffleArray: function (array) {
+      for (var i = array.length - 1; i > 0; i--) {
+        // Generate random number
+        var j = Math.floor(Math.random() * (i + 1));
+
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array;
+    },
+    isTrueRep: function (question, reponse) {
+      for (let q of question.trueRep) {
+        if (q === reponse) {
+          return "success";
+        }
+      }
+      return "error";
+    },
+  },
 };
 </script>
