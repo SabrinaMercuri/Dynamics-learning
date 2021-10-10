24b9dd27800a64613298e748abe9ba4a3f9e72fe
Sabrina <sabrina.mercuri@etu.unice.fr>
2021-10-10 16:40:14 +0200

    ajout de la première page avec le choix enseignant et étudiant

--- dynamics-learning/src/App.vue
+++ dynamics-learning/src/App.vue
@@ -1,42 +1,20 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-  </v-app>
+  <div class="accueil">
+    <div class="logo">
+       <v-img
+        
+        src="./assets/logo.png">
+        </v-img>
+    </div>
+    <div class="buttons">
+      <button class="button1" x-large>
+        Enseignant
+      </button>
+      <button  class="button2" x-large>
+        Etudiant
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -52,3 +30,104 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+
+.accueil {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /*margin-left: 300px;*/
+
+}
+
+.logo {
+  /*background-image: url(./assets/logo.png);
+  background-position: center;
+  background-size: cover;*/
+  align-items: center;
+  width: 40%;
+  height: 40%;
+  display: flex;
+  flex-direction: row;
+  margin-top: 150px;
+  
+}
+
+.buttons {
+  width: 40%;
+  height: 40%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+}
+
+.button1 {
+  width: 500px;
+  height: 120px;
+  margin-right: 150px;
+  font-size: 30px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid black;
+  cursor: pointer;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.button2 {
+  width: 500px;
+  height: 120px;
+  font-size: 30px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid black;
+  cursor: pointer;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.button1::after {
+  content: "";
+  background-color: #5DADE2;
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 15px;
+  left: 15px;
+  transition: 0.2s;
+}
+
+.button2::after {
+  content: "";
+  background-color: #5DADE2;
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 15px;
+  left: 15px;
+  transition: 0.2s;
+}
+
+.button1:hover::after {
+  top: 0px;
+  left: 0px;
+}
+
+.button2:hover::after {
+  top: 0px;
+  left: 0px;
+}
+
+</style>
