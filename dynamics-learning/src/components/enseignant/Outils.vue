7b398bc993b84381c755a36af133f1251e6e2efb
Lindoryel <lindoriel02@gmail.com>
2021-10-11 15:37:04 +0200

    Ajout de tous les icones + centrage des éléments dans la carte

--- dynamics-learning/src/components/enseignant/Outils.vue
+++ dynamics-learning/src/components/enseignant/Outils.vue
@@ -6,39 +6,49 @@
 
         <div class = "outils">
             <v-card id="qcm" elevation="8">
-                <v-card-title>QCM</v-card-title>
+                <v-card-title class="justify-center">QCM</v-card-title>
+                <v-card-text class=text-center>
                 <img src="../../assets/questionnaire.png" alt="Icon" 
                 style="max-height: 50px; max-width: 50px"
-                />
-                <b-button size="lg">Créer</b-button>
+                /><br>
+                <v-btn flat color="orange">Créer</v-btn>
+                </v-card-text>
             </v-card>
             <v-card id="sondage" elevation="8">
-                <v-card-title>Sondage</v-card-title>
+                <v-card-title class="justify-center">Sondage</v-card-title>
+                <v-card-text class=text-center>
                 <img src="../../assets/sondage.png" alt="Icon" 
                 style="max-height: 50px; max-width: 50px"
-                />
-                <b-button disabled size="lg">A venir</b-button>
+                /><br>
+                <v-btn flat color="grey">A venir</v-btn>
+                </v-card-text>
             </v-card>
             <v-card id = "brainstorming" elevation="8">
-                <v-card-title>Brainstorming</v-card-title>
+                <v-card-title class="justify-center">Brainstorming</v-card-title>
+                <v-card-text class=text-center>
                 <img src="../../assets/brainstorming.png" alt="Icon" 
                 style="max-height: 50px; max-width: 50px"
-                />
-                <b-button disabled size="lg">A venir</b-button>
+                /><br>
+                <v-btn flat color="grey">A venir</v-btn>
+                </v-card-text>
             </v-card>
             <v-card id="loadingbar" elevation="8">
-                <v-card-title>Loading Bar</v-card-title>
+                <v-card-title class="justify-center">Loading Bar</v-card-title>
+                <v-card-text class=text-center>
                 <img src="../../assets/loading.png" alt="Icon" 
                 style="max-height: 50px; max-width: 50px"
-                />
-                <b-button disabled size="lg">A venir</b-button>
+                /><br>
+                <v-btn flat color="grey">A venir</v-btn>
+                </v-card-text>
             </v-card>
             <v-card id="text" elevation="8">
-                <v-card-title>Textes à trous</v-card-title>
-                <img :src="selectedImage" alt="Icon" 
-                style="max-height: 300px; max-width: 400px; border: 1px solid black"
-                />
-                <b-button disabled size="lg">A venir</b-button>
+                <v-card-title class="justify-center">Textes à trous</v-card-title>
+                <v-card-text class=text-center>
+                <img src="../../assets/text.png" alt="Icon" 
+                style="max-height: 50px; max-width: 50px"
+                /> <br>
+                <v-btn flat color="grey">A venir</v-btn>
+                </v-card-text>
             </v-card>
         </div>
     </div>
