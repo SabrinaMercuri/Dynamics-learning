ba4b21a91d0c80f392ac06a5327526b446224a19
Sabrina <sabrina.mercuri@etu.unice.fr>
2021-10-13 18:08:35 +0200

    début ajout de la page d'acceuil

--- dynamics-learning/src/App.vue
+++ dynamics-learning/src/App.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="accueil">
     <div class="logo">
-       <v-img
-        
-        src="./assets/logo.png">
-        </v-img>
+      <v-img src="./assets/logo.png"></v-img>
     </div>
-    <div class="buttons">
-      <button class="button1" x-large>
-        Enseignant
-      </button>
-      <button  class="button2" x-large>
-        Etudiant
-      </button>
+    <v-divider></v-divider>
+    <div class="contenu">
+      <v-navigation-drawer permanent width="600">
+        <v-list class="title-menu">
+          <h2>Menu</h2>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list nav dense>
+          <v-list-item @click="mesoutils()" link class="menu-items">
+            <v-list-item-title class="menu-props">Mes outils</v-list-item-title>
+          </v-list-item>
+          <v-list-item class="menu-items">
+            <v-list-item-title class="menu-props">Mes cours</v-list-item-title>
+          </v-list-item>
+          <v-list-item class="menu-items">
+            <v-list-item-title class="menu-props">Mes statistiques</v-list-item-title>
+          </v-list-item>
+          <v-list-item class="menu-items">
+            <v-list-item-title class="menu-props">Paramètres</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-divider vertical></v-divider>
+      <div class="outils">
+        Les outils
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
-};
 </script>
 
 <style scoped>
-
 .accueil {
+  
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  /*align-items: center;*/
+  /*justify-content: center;*/
   /*margin-left: 300px;*/
-
+  min-height: 100vh;
 }
 
 .logo {
@@ -49,85 +54,46 @@ export default {
   background-position: center;
   background-size: cover;*/
   align-items: center;
-  width: 40%;
-  height: 40%;
+  justify-content: center;
+  width: 30%;
+  height: 30%;
   display: flex;
-  flex-direction: row;
-  margin-top: 150px;
+  /*flex-direction: row;*/
+  margin-top: 30px;
+  margin-left: 30%;
+}
+
+.contenu {
+  display: flex;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  background-color: azure;
+}
+
+.title-menu {
+  text-align: center;
+  /*height: 5%;*/
+  font-family: Georgia, "Times New Roman", Times, serif;
+  margin-top: 4%;
+  font-size: 40px;
+}
+
+.menu-props {
+  text-align: center;
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 2;
+  font-size: large;
+}
+
+.menu-items {
+  margin-top: 15%;
   
 }
 
-.buttons {
-  width: 40%;
-  height: 40%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin-top: 100px;
-}
-
-.button1 {
-  width: 500px;
-  height: 120px;
-  margin-right: 150px;
-  font-size: 30px;
-  font-weight: 200;
-  letter-spacing: 1px;
-  padding: 13px 50px 13px;
-  outline: 0;
-  border: 1px solid black;
-  cursor: pointer;
-  position: relative;
-  background-color: rgba(0, 0, 0, 0);
-}
-
-.button2 {
-  width: 500px;
-  height: 120px;
-  font-size: 30px;
-  font-weight: 200;
-  letter-spacing: 1px;
-  padding: 13px 50px 13px;
-  outline: 0;
-  border: 1px solid black;
-  cursor: pointer;
-  position: relative;
-  background-color: rgba(0, 0, 0, 0);
-}
-
-.button1::after {
-  content: "";
-  background-color: #5DADE2;
-  width: 100%;
-  z-index: -1;
-  position: absolute;
-  height: 100%;
-  top: 15px;
-  left: 15px;
-  transition: 0.2s;
-}
-
-.button2::after {
-  content: "";
-  background-color: #5DADE2;
-  width: 100%;
-  z-index: -1;
-  position: absolute;
-  height: 100%;
-  top: 15px;
-  left: 15px;
-  transition: 0.2s;
-}
-
-.button1:hover::after {
-  top: 0px;
-  left: 0px;
-}
-
-.button2:hover::after {
-  top: 0px;
-  left: 0px;
+.outils{
+  border-left-style: groove;
+  border-width:0.5ex;
 }
 
 </style>
---
+++ dynamics-learning/src/components/etudiant/accueil.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="accueil">
+    <div class="logo">
+       <v-img
+        src="./assets/logo.png">
+        </v-img>
+    </div>
+    <div class="buttons">
+      <button class="button1" x-large>
+        Enseignant
+      </button>
+      <button  class="button2" x-large>
+        Etudiant
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+
+.accueil {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /*margin-left: 300px;*/
+
+}
+
+.logo {
+  /*background-image: url(./assets/logo.png);
+  background-position: center;
+  background-size: cover;*/
+  align-items: center;
+  width: 40%;
+  height: 40%;
+  display: flex;
+  flex-direction: row;
+  margin-top: 150px;
+}
+
+.buttons {
+  width: 40%;
+  height: 40%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+}
+
+.button1 {
+  width: 500px;
+  height: 120px;
+  margin-right: 150px;
+  font-size: 30px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid black;
+  cursor: pointer;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.button2 {
+  width: 500px;
+  height: 120px;
+  font-size: 30px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid black;
+  cursor: pointer;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.button1::after {
+  content: "";
+  background-color: #5DADE2;
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 15px;
+  left: 15px;
+  transition: 0.2s;
+}
+
+.button2::after {
+  content: "";
+  background-color: #5DADE2;
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 15px;
+  left: 15px;
+  transition: 0.2s;
+}
+
+.button1:hover::after {
+  top: 0px;
+  left: 0px;
+}
+
+.button2:hover::after {
+  top: 0px;
+  left: 0px;
+}
+
+</style>
